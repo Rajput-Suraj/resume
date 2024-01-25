07c768d846b3a2ebf267cac2930828279a7366e7
Rajput-Suraj <surajarajput@gmail.com>
2024-01-25 19:53:31 +0530

    Created a section component

---
+++ src/components/Section.tsx
@@ -0,0 +1,24 @@
+function Section() {
+  return (
+    <>
+      <div className="section">
+        <div className="text-upper">Experience</div>
+        <div className="">
+          <div className="section-wrapper mb-40">
+            <div className="section-details">
+              <div className="title">Enquero - React Developer</div>
+              <div className="sub-title">Jun 2022 - Present | Karnataka, India</div>
+            </div>
+            <div>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quas repudiandae cupiditate,
+              sapiente eligendi adipisci repellat, voluptatum quisquam quibusdam dicta earum ab est
+              necessitatibus accusamus facere itaque cumque veniam modi porro.
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Section;
